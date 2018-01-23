bb98c65f57efa201da995743f7706f229e514ff6
xueyufei <yunruofengsheng@126.com>
2018-01-23 10:23:23 +0800

    'move'

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import moment from 'moment';
 import readme from '../../README.md' // 解析markdown
-import Input from '../../copy/input.jsx'
+import Input from '../../node/copy/input.jsx'
 import React from 'react'
 import Router from 'koa-router'
 import { render } from 'react-dom'
